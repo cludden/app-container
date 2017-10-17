36463c07ba49d03bb5ae2387d4cf8ce7353983ed
chris ludden <chris.ludden@gmail.com>
2017-10-17 17:27:41 -0600

    feat: adds `register` method to container for manually registering components

---
+++ test/tests/manual.test.js
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import Container from '../../lib/container';
+import * as amod from '../fixtures/namespaced/a';
+import * as bmod from '../fixtures/namespaced/b';
+import * as dmod from '../fixtures/namespaced/d';
+
+describe('manual', function () {
+  it('should allow components to be registered manually', async function () {
+    const container = new Container({
+      namespace: 'inject',
+      defaults: { singleton: true },
+    });
+    container.register(amod, 'a', amod.inject);
+    container.register(bmod, 'b', bmod.inject);
+    container.register(dmod, 'd', dmod.inject);
+    const [a, b, d] = await container.load('a', 'b', 'd');
+    expect(a).to.have.property('doSomething');
+    expect(b).to.have.property('doSomethingElse');
+    expect(d).to.have.property('bar');
+  });
+});
